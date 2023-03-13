33a4a3050adf07d109ffd93c7ec2aa6956c28351
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-13 14:45:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// currentUser  is defined
+const currentUser = "Sarah";
+
+// welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
